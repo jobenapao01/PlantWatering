4639d463f9fd646224c05ece94de7c32e5e6b3e8
Joben Apao <jobenapao10@gmail.com>
2019-03-01 03:28:02 +0800

    FIXED BUG WHERE ESP8266 RESETS

--- PlantWatering.ino
+++ PlantWatering.ino
@@ -4,7 +4,6 @@
 #include <FirebaseArduino.h>
 #include <ArduinoJson.h>
 #include <string>
-using namespace std;
 
 #define WIFI_SSID "SHEENA" //Wifi SSID
 #define WIFI_PASSWORD "HYDROCORTISONE2005" //Wifi password
@@ -13,15 +12,13 @@ using namespace std;
 #define FIREBASE_AUTH "75rzCPuqnO0hiRjwAIcTf6Numsdc1Kn7ghIXftfn"
 
 #define pump 14 //d5
-#define soilMoisture 13 //D7
-#define waterSensor 15 // D8
+#define soilMoisture 15 //D8
+#define waterSensor A0 // Analog pin 0
 
 
 WiFiClient client;
 int soilOutput = 0;
 const long utcOffsetInSeconds = 28800;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
 
 
 // Define NTP Client to get time
@@ -29,50 +26,43 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp.pagasa.dost.gov.ph", utcOffsetInSeconds);
 
 void setup() {
-  Serial.begin(115290);
+  Serial.begin(115200);
   initializeWifi();
   initializePins();
-  sendSoilData();
   timeClient.begin();
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+ 
 }
 
 void loop() {
   timeClient.update();
-  blinkLed();
-  sendSoilData();
+   blinkLed();
   pumpPower();
   triggerSched();
+  sendSoilData();
+  waterValue();
+  
 }
 
-void timeDisplay() {
-  //FirebaseObject obj = Firebase.get("MainSchedule");
-  Serial.print(daysOfTheWeek[timeClient.getDay()]);
- 
-}
 void pumpPower() {
-  //TODO: save logs when pump is on
-  int pumpStatus = Firebase.getString("Pump/pumpState").toInt();
-  Serial.println(pumpStatus);
+   int pumpStatus = Firebase.getString("Pump/pumpState").toInt();
   if (pumpStatus == 1) {
-    pumpOn();
-  } else if (pumpStatus == 0) {
-    pumpOff();
-  }
+      pumpOn();
+    } else {
+      pumpOff(); 
+    }
 }
 
 void pumpOn(){
-  if(digitalRead(soilMoisture) == HIGH){
+  while(digitalRead(soilMoisture) == HIGH){
     digitalWrite(pump, HIGH);
     Firebase.setString("Pump/pumpState", "1");
-    delay(1000);
-  }
-  
-  else if(digitalRead(soilMoisture) == LOW){
-         digitalWrite(pump,LOW);
-         Firebase.setString("Pump/pumpState", "0");
-         delay(1000);
+
+    if(digitalRead(soilMoisture) == LOW){
+         pumpOff();     
+         break;
     }
+  } 
 }
 
 void pumpOff(){
@@ -81,12 +71,15 @@ void pumpOff(){
 }
 
 void waterValue() {
-  int waterLevel = digitalRead(waterSensor);
-
-  Serial.print("Water Level: ");
-  Serial.print(waterLevel);
-  Serial.println("%");
-  delay(100);
+   int waterLevel = analogRead(waterSensor);
+   delay(1);
+   int waterPercent = (waterLevel/100)*100;
+   if(waterPercent == 0){
+    Firebase.setString("Water/waterValue", "EMPTY");
+   }else{
+    Firebase.setString("Water/waterValue","FILLED");
+   }
+  
 }
 
 void blinkLed() {
@@ -99,7 +92,7 @@ void blinkLed() {
 void sendSoilData() {
   if (digitalRead(soilMoisture) == HIGH) {
     Serial.println("DRY");
-    Firebase.setString("Soil/soilValue", "DRY");
+     Firebase.setString("Soil/soilValue", "DRY");
     delay(2000);
   }
   //Firebase.setInt();
@@ -130,6 +123,7 @@ void initializePins() {
   pinMode(pump, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(soilMoisture, INPUT);
+  pinMode(waterSensor, INPUT);
 }
 
 void triggerSched(){
@@ -159,159 +153,192 @@ void triggerSched(){
   }
 }
 
-void mondaySched(){
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String monday = obj.getString("monday");
-  String m[monday.length()];
-  int m1 = 0;
-   for (int i = 0; i < monday.length(); i++) {
-    if (monday.substring(i, i+1) == ",") {
-      m[m1] = monday.substring(i-5,i);
-      m1++;
+void mondaySched(){  
+    FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String monday = obj.getString("monday");
+    if(monday.length() > 0){
+      String m[monday.length()];
+    int m1 = 0;
+     for (int i = 0; i < monday.length(); i++) {
+      if (monday.substring(i, i+1) == ",") {
+        m[m1] = monday.substring(i-5,i);
+        m1++;
+      }
     }
-  }
-  m1++;
-  m[m1] = monday.substring(monday.length()-5, monday.length());
-  for (int i = 0; i <= m1; i++) {
-    if (curTime == m[i]) {
-      pumpOn();
+    m1++;
+    m[m1] = monday.substring(monday.length()-5, monday.length());
+    for (int i = 0; i <= m1; i++) {
+      if (curTime == m[i]) {
+        pumpOn();
+      }else{
+      pumpOff();
+      }
     }
-  }
+   }
+    
 }
 
 void tuesdaySched(){
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String tuesday = obj.getString("tuesday");
-  String t[tuesday.length()];
-  int t1 = 0;
-   for (int i = 0; i < tuesday.length(); i++) {
-    if (tuesday.substring(i, i+1) == ",") {
-      t[t1] = tuesday.substring(i-5,i);
-      t1++;
+    FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String tuesday = obj.getString("tuesday");
+    if(tuesday.length() > 0){
+       String t[tuesday.length()];
+    int t1 = 0;
+     for (int i = 0; i < tuesday.length(); i++) {
+      if (tuesday.substring(i, i+1) == ",") {
+        t[t1] = tuesday.substring(i-5,i);
+        t1++;
+      }
     }
-  }
-  t1++;
-  t[t1] = tuesday.substring(tuesday.length()-5, tuesday.length());
-  for (int i = 0; i <= t1; i++) {
-    if (curTime == t[i]) {
-      pumpOn();
+    t1++;
+    t[t1] = tuesday.substring(tuesday.length()-5, tuesday.length());
+    for (int i = 0; i <= t1; i++) {
+      if (curTime == t[i]) {
+        pumpOn();
+      }else{
+       pumpOff();
+      }
     }
-  }
+   }
+   
 }
 
-void wednesdaySched(){  
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String wednesday = obj.getString("wednesday");
-  String w[wednesday.length()];
-  int w1 = 0;
-   for (int i = 0; i < wednesday.length(); i++) {
-    if (wednesday.substring(i, i+1) == ",") {
-      w[w1] = wednesday.substring(i-5,i);
+void wednesdaySched(){ 
+    FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String wednesday = obj.getString("wednesday");
+    if(wednesday.length() > 0){
+      String w[wednesday.length()];
+      int w1 = 0;
+       for (int i = 0; i < wednesday.length(); i++) {
+        if (wednesday.substring(i, i+1) == ",") {
+          w[w1] = wednesday.substring(i-5,i);
+          w1++;
+        }
+      }
       w1++;
+      w[w1] = wednesday.substring(wednesday.length()-5, wednesday.length());
+      for (int i = 0; i <= w1; i++) {
+        if (curTime == w[i]) {
+          pumpOn();
+        }else{
+          pumpOff();
+        }
+      }
     }
-  }
-  w1++;
-  w[w1] = wednesday.substring(wednesday.length()-5, wednesday.length());
-  for (int i = 0; i <= w1; i++) {
-    if (curTime == w[i]) {
-      pumpOn();
-    }else{
-      pumpOff();
-    }
-  }
+    
 }
 
 void thursdaySched(){
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String thursday = obj.getString("thursday");
-  String th[thursday.length()];
-  int th1 = 0;
-   for (int i = 0; i < thursday.length(); i++) {
-    if (thursday.substring(i, i+1) == ",") {
-      th[th1] = thursday.substring(i-5,i);
-      th1++;
+     FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String thursday = obj.getString("thursday");
+    if(thursday.length() > 0){
+       String th[thursday.length()];
+    int th1 = 0;
+     for (int i = 0; i < thursday.length(); i++) {
+      if (thursday.substring(i, i+1) == ",") {
+        th[th1] = thursday.substring(i-5,i);
+        th1++;
+      }
     }
-  }
-  th1++;
-  th[th1] = thursday.substring(thursday.length()-5, thursday.length());
-  for (int i = 0; i <= th1; i++) {
-    if (curTime == th[i]) {
-      pumpOn();
-    }
-  }
+    th1++;
+    th[th1] = thursday.substring(thursday.length()-5, thursday.length());
+    for (int i = 0; i <= th1; i++) {
+      if (curTime == th[i]) {
+        pumpOn();
+        Serial.println(th[i]);
+      }else{
+      pumpOff();
+       }
+     } 
+   }
+   
 }
 
 void fridaySched(){
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String friday = obj.getString("friday");
-  String f[friday.length()];
-  int f1 = 0;
-   for (int i = 0; i < friday.length(); i++) {
-    if (friday.substring(i, i+1) == ",") {
-      f[f1] = friday.substring(i-5,i);
-      f1++;
+     FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String friday = obj.getString("friday");
+    if(friday.length() > 0){
+      String f[friday.length()];
+    int f1 = 0;
+     for (int i = 0; i < friday.length(); i++) {
+      if (friday.substring(i, i+1) == ",") {
+        f[f1] = friday.substring(i-5,i);
+        f1++;
+      }
     }
-  }
-  f1++;
-  f[f1] = friday.substring(friday.length()-5, friday.length());
-   for (int i = 0; i <= f1; i++) {
-    if (curTime == f[i]) {
-      pumpOn();
-    }
-      
-  }
+    f1++;
+    f[f1] = friday.substring(friday.length()-5, friday.length());
+     for (int i = 0; i <= f1; i++) {
+      if (curTime == f[i]) {
+        pumpOn();
+      }else{
+      pumpOff();
+       }
+     }
+   }
+    
 }
 
 void saturdaySched(){
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String saturday = obj.getString("saturday");
-  String s[saturday.length()];
-  int s1 = 0;
-   for (int i = 0; i < saturday.length(); i++) {
-    if (saturday.substring(i, i+1) == ",") {
-      s[s1] = saturday.substring(i-5,i);
+     FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String saturday = obj.getString("saturday");
+    if(saturday.length()){
+      String s[saturday.length()];
+      int s1 = 0;
+       for (int i = 0; i < saturday.length(); i++) {
+        if (saturday.substring(i, i+1) == ",") {
+          s[s1] = saturday.substring(i-5,i);
+          s1++;
+        }
+      }
       s1++;
+      s[s1] = saturday.substring(saturday.length()-5, saturday.length());
+      for (int i = 0; i <= s1; i++) {
+        if (curTime == s[i]) {
+          pumpOn();
+        }else{
+        pumpOff();
+        }
+      }
     }
-  }
-  s1++;
-  s[s1] = saturday.substring(saturday.length()-5, saturday.length());
-  for (int i = 0; i <= s1; i++) {
-    if (curTime == s[i]) {
-      pumpOn();
-    }
-  }
+    
 }
 
 void sundaySched(){
-  FirebaseObject obj = Firebase.get("MainSchedule");
-  String currentTime = timeClient.getFormattedTime();
-  String curTime = currentTime.substring(0,5);
-  String sunday = obj.getString("sunday");
-  String su[sunday.length()];
-  int su1 = 0;
-   for (int i = 0; i < sunday.length(); i++) {
-    if (sunday.substring(i, i+1) == ",") {
-      su[su1] = sunday.substring(i-5,i);
+     FirebaseObject obj = Firebase.get("MainSchedule");
+    String currentTime = timeClient.getFormattedTime();
+    String curTime = currentTime.substring(0,5);
+    String sunday = obj.getString("sunday");
+    if(sunday.length() > 0){
+       String su[sunday.length()];
+      int su1 = 0;
+       for (int i = 0; i < sunday.length(); i++) {
+        if (sunday.substring(i, i+1) == ",") {
+          su[su1] = sunday.substring(i-5,i);
+          su1++;
+        }
+      }
       su1++;
+      su[su1] = sunday.substring(sunday.length()-5, sunday.length());
+      for (int i = 0; i <= su1; i++) {
+        if (curTime == su[i]) {
+          pumpOn();
+        }else{
+        pumpOff();
+        }
+      }
     }
-  }
-  su1++;
-  su[su1] = sunday.substring(sunday.length()-5, sunday.length());
-  for (int i = 0; i <= su1; i++) {
-    if (curTime == su[i]) {
-      pumpOn();
-    }
-  }
+   
 }
